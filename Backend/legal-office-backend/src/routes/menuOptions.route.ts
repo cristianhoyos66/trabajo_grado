f0cb9d987dc902aeca6e17eab3994b5261f64070
cristianhoyos66 <cristianhoyos66@gmail.com>
2022-10-16 19:03:22 -0500

    Add query param to send URL
    This query param will be used to check if the URL exists for the
    specific role.

--- Backend/legal-office-backend/src/routes/menuOptions.route.ts
+++ Backend/legal-office-backend/src/routes/menuOptions.route.ts
@@ -4,9 +4,9 @@ import asyncHandler from 'express-async-handler'
 
 const router = express.Router()
 
-router.get('/', asyncHandler(async (_req, res, next): Promise<any> => {
+router.get('/', asyncHandler(async (req, res, next): Promise<any> => {
   try {
-    const response = await menuOptionsService.getAll(res.locals.userId)
+    const response = await menuOptionsService.getAll(res.locals.userId, req.query.url)
     res.send(response)
   } catch (error) {
     next(error)
--- Backend/legal-office-backend/src/services/menuOptions.service.ts
+++ Backend/legal-office-backend/src/services/menuOptions.service.ts
@@ -2,8 +2,15 @@ import Permission from '../models/permission.model'
 import PermissionRole, { PermissionRoleAttr } from '../models/permissionRole.model'
 import * as userService from '../services/user.service'
 
-export const getAll = async (userId: bigint): Promise<PermissionRoleAttr[]> => {
+export const getAll = async (userId: bigint, url: any): Promise<PermissionRoleAttr[]> => {
   const user: any = await userService.getById(userId.toString())
+  const whereClause: {'$permission.action$': any, roleId: bigint, '$permission.url$'?: string } = {
+    '$permission.action$': null,
+    roleId: user?.roleId
+  }
+  if (url !== undefined) {
+    whereClause['$permission.url$'] = url
+  }
   const areas: PermissionRoleAttr[] = await PermissionRole.findAll({
     include: [
       {
@@ -12,10 +19,7 @@ export const getAll = async (userId: bigint): Promise<PermissionRoleAttr[]> => {
         as: 'permission'
       }
     ],
-    where: {
-      '$permission.action$': null,
-      roleId: user?.roleId
-    }
+    where: whereClause
   })
   return areas
 }
